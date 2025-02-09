5e677c3e0f787d169ac1758089a3a84832927912
Kristian Dimitrov <krisitoo10@gmail.com>
2025-02-09 18:53:21 +0200

    Add CRUD operations functionality for Airport slots

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
 
 const authRouter = require('./auth');
+const airportSlotsRouter = require('./airportSlots');
 
-router.get('/', (req, res) => {
+router.get('/', (_, res) => {
     res.render('index', { title: 'Home Page' });
 });
 
 router.use('/auth', authRouter);
+router.use('/airport-slots', airportSlotsRouter);
 
 module.exports = router;
