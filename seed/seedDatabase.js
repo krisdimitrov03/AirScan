f1fd434a2a615ca6a25016232d5629b46b752376
Kristian Dimitrov <krisitoo10@gmail.com>
2025-02-10 14:17:10 +0200

    Fix seeding functionality

--- seed/seedDatabase.js
+++ seed/seedDatabase.js
@@ -9,19 +9,37 @@ const {
   Pricing,
 } = require("../models");
 
-const seedTable = async (model, dataFilePath, modelName) => {
+const seedTable = async (
+  model,
+  dataFilePath,
+  modelName,
+  flights = undefined
+) => {
   try {
     const count = await model.count();
     if (count === 0) {
-      const data = loadJsonData(dataFilePath);
-      await model.bulkCreate(data);
+      let data = loadJsonData(dataFilePath);
+
+      if (flights) data = await mapToFlights(data, flights);
+
+      const returnedData = await model.bulkCreate(data);
       console.log(`✅ ${modelName} seeded successfully`);
+      return returnedData;
     } else {
       console.log(`⚠️ ${modelName} already exists, skipping seeding`);
+      return [];
     }
   } catch (error) {
     console.error(`❌ Error seeding ${modelName}:`, error);
+    return [];
   }
+};
+
+const mapToFlights = async (records, flights) => {
+  for (let index = 0; index < records.length; index++)
+    records[index].flight_id = flights[index].flight_id;
+
+  return records;
 };
 
 const seedDatabase = async () => {
@@ -30,15 +48,17 @@ const seedDatabase = async () => {
 
     await seedTable(Role, "./data/roles.json", "Roles");
     await seedTable(User, "./data/users.json", "Users");
-    await seedTable(Flight, "./data/flights.json", "Flights");
     await seedTable(AirportSlot, "./data/airport_slots.json", "Airport Slots");
     await seedTable(Event, "./data/events.json", "Events");
+
+    const flights = await seedTable(Flight, "./data/flights.json", "Flights");
     await seedTable(
       DemandHistory,
       "./data/demand_history.json",
-      "Demand History"
+      "Demand History",
+      flights
     );
-    await seedTable(Pricing, "./data/pricing.json", "Pricing");
+    await seedTable(Pricing, "./data/pricing.json", "Pricing", flights);
   } catch (error) {
     console.error("❌ Error seeding database:", error);
   }
